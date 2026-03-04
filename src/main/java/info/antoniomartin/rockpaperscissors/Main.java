2dc7cd646b25666237756a7243c8e0615abe74b4
Antonio Martin <antoniomartinescalera@gmail.com>
2026-03-04 09:56:11 +0100

    Fix small error in Gradle and improve print result

--- src/main/java/info/antoniomartin/rockpaperscissors/Main.java
+++ src/main/java/info/antoniomartin/rockpaperscissors/Main.java
@@ -1,16 +1,20 @@
 package info.antoniomartin.rockpaperscissors;
 
 
+import static java.lang.String.format;
+import static java.lang.System.out;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Rock Paper Scissors!");
+        out.println("Welcome to Rock Paper Scissors!");
         Player playerA = new Player("Machine", Move.PAPER);
         Player playerB= new Player("Antonio");
 
         Game game = new Game(playerA, playerB, 100);
 
+        out.println("Playing....");
         GameReport result = game.play();
 
         printResults(playerA.getPlayerName(), playerB.getPlayerName(), result);
@@ -18,10 +22,10 @@ public class Main {
     }
 
     private static void printResults(String aName, String bName, GameReport r) {
-        System.out.println("===== FINAL RESULTS =====");
-        System.out.printf("%s - Victory: %d | Lost: %d | Draws: %d%n",
-                aName, r.aWins(), r.bWins(), r.draws());
-        System.out.printf("%s - Victory: %d | Lost: %d | Draws: %d%n",
-                bName, r.bWins(), r.aWins(), r.draws());
+        out.println(format("""
+        ===== FINAL RESULTS ===
+        %s - Victory: %d | Lost: %d | Draws: %d
+        %s - Victory: %d | Lost: %d | Draws: %d
+        """,aName, r.aWins(), r.bWins(), r.draws(), bName, r.bWins(), r.aWins(), r.draws()));
     }
 }
